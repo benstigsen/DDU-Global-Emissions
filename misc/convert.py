65520be65b38ae6188ed4535736d0dc916c231e0
BenStigsen <benjamin.stigsen@gmail.com>
2021-03-18 19:56:38 +0100

    Added scripts and data

---
+++ misc/convert.py
@@ -0,0 +1,28 @@
+import json
+
+codes = None
+
+with open("iso3.json", "r") as file:
+  codes = json.load(file)
+
+with open("data.json", "r") as file:
+  data = file.read()
+  
+  for key in codes:
+    data = data.replace(f"\"{codes[key]}", f"\"{key}")
+  
+  data = json.loads(data)
+  
+  output = ""
+  for i in data:
+    code = i["Code"]
+    value = str(i["Annual CO2 emissions"])
+    
+    if len(code) == 2 and not "E" in value:
+      output += f'{code}: {{emission: {value}}},\n'
+  
+  with open("test.txt", "w") as out:
+    out.write(output)
+    
+  print(output)
+
